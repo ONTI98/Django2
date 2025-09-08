879ee3f97573b845f2bda54d726e823a301f85aa
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-09-08 22:08:02 +0200

    Add datetimefield

--- feed/models.py
+++ feed/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Post(models.Model):
-    text=models.CharField(max_length=255)
+    text=models.CharField(max_length=240,blank=False,null=False)
+    date=models.DateTimeField(auto_now=True,blank=False)
+
+    def __str__(self):
+        return (self.text)
