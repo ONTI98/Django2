a564f4c13f5c8a67d7a17c41bd28667a687c98d4
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-09-08 22:07:36 +0200

    Create a Homepage view

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Post
+from django.views.generic import *
 # Create your views here.
+
+
+
+class HomePage(ListView):
+    model=Post
+    template_name="homepage.html"
+    context_object_name="posts"
+    http_method_names=["get"]
+    queryset=Post.objects.all().order_by("-id") #arranges posts by descending order
+
