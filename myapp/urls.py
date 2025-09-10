9bd3b912c6191c2271377d9a97c1a1d0490c5284
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-09-10 13:41:19 +0200

    Create url for allauth

---
+++ myapp/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+from feed import urls as feed_urls
+from django.conf.urls import include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include(feed_urls,namespace="feed")),
+    path('',include("allauth.urls"))
+]
+ 
