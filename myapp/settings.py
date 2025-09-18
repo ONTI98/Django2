58be4423a0f046dc20c625e2c8ccab3cd1e76ede
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-09-18 09:00:29 +0200

    add image column to profile

--- myapp/settings.py
+++ myapp/settings.py
@@ -5,6 +5,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -34,7 +37,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'tailwind',
     'theme',
-
     'profiles',
     'sorl.thumbnail'
 ]
