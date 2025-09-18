b4ce3bdf069b669552abfd4650b62f11eff86555
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-09-18 08:56:14 +0200

    add authour column to Post model

--- feed/models.py
+++ feed/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -6,6 +7,9 @@ from django.db import models
 class Post(models.Model):
     text=models.CharField(max_length=240,blank=False,null=False)
     date=models.DateTimeField(auto_now=True,blank=False)
-
+    author=models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
     def __str__(self):
         return (self.text)
