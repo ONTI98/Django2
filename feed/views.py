5b695905614432774bab7793f38135031fc9b423
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-09-18 13:48:34 +0200

    create PostDeatilView class

--- feed/views.py
+++ feed/views.py
@@ -13,3 +13,8 @@ class HomePage(ListView):
     http_method_names=["get"]
     queryset=Post.objects.all().order_by("-id")[0:30] #arranges posts by descending order
 
+class PostDetailView(DetailView):
+    http_method_names=["get"]
+    template_name="feed/detail.html"
+    model=Post
+    context_object_name="post"
