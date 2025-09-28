243555cc20d99f3dd2cc3bfec25b48a6be875599
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-09-28 22:07:06 +0200

    create post method for the ajax request

--- feed/views.py
+++ feed/views.py
@@ -5,7 +5,7 @@ from django.views.generic import *
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin #will force user to be authenticated
 # Create your views here.
-
+from django.shortcuts import render
 
 
 class HomePage(ListView):
@@ -37,5 +37,19 @@ class CreateNewPostView(LoginRequiredMixin,CreateView):
         obj.save()
         return super().form_valid(form)
     
-
+    #create post method
+    def post(self,request,*args,**kwargs):
+       #create a new post object
+       post=Post.objects.create(
+           text=request.POST.get("text"),
+           author=request.user,
+           )
+       return render(
+           request,"includes/post.html",
+           {
+               "post":post, #from post.html
+               "show_detail_link":True,
+           },
+           content_type="application/html"
+           )
 
