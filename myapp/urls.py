e18a12d7dbb4da11bd3d2086144237ffe24619b9
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-09-30 15:13:19 +0200

    add new url  profile details

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,14 +5,18 @@ from feed import urls as feed_urls
 from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from profiles import urls as profile_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(feed_urls,namespace="feed")),
     path('',include("allauth.urls")),
+    path('',include(profile_urls,namespace="profiles"))
 
     ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
+
+#realoding django
 if settings.DEBUG:
     urlpatterns+=[
         path('__reload__/',include("django_browser_reload.urls"))
