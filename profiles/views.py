25074cda581a491827a61dd391061f81736ffd94
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-10-01 13:42:01 +0200

    get total number of posts

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views.generic import DetailView
+from feed.models import Post
 
 # Create your views here.
 
@@ -14,5 +15,9 @@ class ProfileDetailView(DetailView):
     slug_url_kwarg="username"
 
 
-
-
+    #get total number of posts
+    def get_context_data(self,**kwargs):
+        user=self.get_object()
+        context=super().get_context_data(**kwargs)
+        context['total_posts']=Post.objects.filter(author=user).count()
+        return context
