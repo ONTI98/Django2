6f839d042e9ea9fd288f682e14da971076f470bf
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-10-04 13:25:49 +0200

    add follow url

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from  .views import *
 
 app_name="profile"
 
 urlpatterns=[
-    path("profile/<str:username>",ProfileDetailView.as_view(),name="profile_details")
+    path("profile/<str:username>",ProfileDetailView.as_view(),name="profile_details"),
+    path("profile/<str>:username/follow/",FollowView.as_view(),name="follow" )
 ]
