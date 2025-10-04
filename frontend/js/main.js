69777d44deda791c203c749da9fc5507e533fb2e
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-10-04 22:25:41 +0200

    follow button data-action dynamic change

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -82,20 +82,25 @@ $(document).on("click",".js-submit",function(event){
 
 
   $Followbutton=$(".js-follow-button") //vraiable for the follow button
-
+  
   $($Followbutton).on("click",function(event){
     event.preventDefault()
+    let action=$(this).attr("data-action") //make action a variable
 
-    console.log("follow this user")
     $.ajax({
             type:"POST",
             url:$(this).data("url"),
             data:{
-                action:$(this).attr("data-action"), //we use this because we do not want caching
+                action:action, //we use this because we do not want caching
                 username:$(this).data("username")  //look for username./Here we do not need to worry about caching
             },
             success:(data)=>{
-
+                $(this).text(data.wording)        //change the follow button text to the 'unfollow' wording
+                if (action =="follow"){
+                    $(this).attr("data-action","unfollow")
+                }else{
+                    $(this).attr("data-action","follow")
+                }
             }, 
             error:(error)=>{
                
