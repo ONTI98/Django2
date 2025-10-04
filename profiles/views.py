b3bd218bffb442ff40b5174e4a15513f62420c00
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-10-04 15:14:46 +0200

    add a post method

--- profiles/views.py
+++ profiles/views.py
@@ -5,7 +5,8 @@ from feed.models import Post
 from followers.models import Follower
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
-
+from django.http import JsonResponse
+from django.http import HttpResponseBadRequest
 # Create your views here.
 
 class ProfileDetailView(DetailView):
@@ -29,4 +30,38 @@ class ProfileDetailView(DetailView):
         return context
 
 class FollowView(LoginRequiredMixin,View):
-        pass
+        http_method_names=["post"]
+        
+        def post(self,request,*args,**kwargs):
+             
+             data=request.POST.dict()
+             if "action" not in data or "username" not in data:
+                  return HttpResponseBadRequest("Missing data")
+             try:
+                   other_user=User.objects.get(username=data["username"])
+             
+             except  User.DoesNotExist:
+                  return HttpResponseBadRequest("User not found")
+             if data['action'] == "follow":
+                  follower,created=Follower.objects.get_or_create(
+                  followed_by=request.user,
+                  following=other_user)
+
+             else:
+                    try:
+                       folower=Follower.objects.get(
+                            followed_by=request.user,
+                            following=other_user,
+                       )
+                    except Follower.DoesNotExist:
+                         follower=None 
+                    if follower:
+                         follower.delete()
+
+             return JsonResponse(
+              {
+                   "success":True,
+                   "wording":"unfollow" if data["action"] =="follow" else  data["action"]=="Follow"
+              }
+             )
+          
