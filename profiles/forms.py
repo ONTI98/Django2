7d5da4dd5253919bcbc4d4c92f950bb19d0df098
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-10-12 13:52:40 +0200

    update user  information

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,9 +1,23 @@
-
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.models import  User
+from .models import Profile
 
-class UserUpdateForm(forms.ModelForm):
+
+
+#update first_name,last_name and email. This is linked to the User model
+class UpdateUserDetails(forms.ModelForm):
+
+     first_name=forms.CharField( max_length=240,
+                                required=True,
+                                widget=forms.TextInput(attrs={'class':'form-control'}))
+     last_name=forms.CharField( max_length=240,
+                                required=True,
+                                widget=forms.TextInput(attrs={'class':'form-control'}))
+     email=forms.EmailField( max_length=240,
+                                required=True,
+                                widget=forms.TextInput(attrs={'class':'form-control'}))
+     
      class Meta:
           model=User
-          fields=["first_name","last_name","email"]
-          
+          fields=['first_name','last-name','email']
+
