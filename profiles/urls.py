4ded3d7f0a55e7299cf07a55a4046722242fe9a9
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-10-17 13:12:48 +0200

    update user information url

--- profiles/urls.py
+++ profiles/urls.py
@@ -9,6 +9,6 @@ urlpatterns=[
     path("profile/<str>:username/follow/",FollowView.as_view(),name="follow" ),
     path("profile/",ProfileView.as_view(),name="profile"),
     path("change_password/",ChangePasswordView.as_view(),name="password"),
-    path("update_user/",update_profile_information,name="update"),
-    
+    path("update_user/",update_profile_information,name="update_user_details"),
+    path("update_avatar/",update_profile_information,name="update_avatar")
     ]
