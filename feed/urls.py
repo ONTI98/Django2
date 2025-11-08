1f3c0920f1dd1c08b72eaeef7ec351920ed71f7e
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-11-08 12:24:47 +0200

    add discover view url

--- feed/urls.py
+++ feed/urls.py
@@ -6,6 +6,7 @@ app_name="feed"
 
 urlpatterns=[path('',HomePage.as_view(),name="index"),
             path('post/<int:pk>/',PostDetailView.as_view(),name="post_details"),
-            path('new',CreateNewPostView.as_view(),name="new_post")
+            path('new',CreateNewPostView.as_view(),name="new_post"), 
+            path('discover',DiscoverView.as_view(),name="discover")
               ]
 
